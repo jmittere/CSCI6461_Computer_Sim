d468f688e081160f956b0cd81e5fd585e58ee68a
jmittere <justin.mittereder@gmail.com>
2025-01-27 20:19:43 -0500

    added method to do first pass to get label values and testing code from main

--- Main.java
+++ Main.java
@@ -5,13 +5,8 @@ public class Main {
 
 	public static void main(String[] args){
 
-		System.out.println("Hello, World!");
 		String sourceFile = "SourceFiles/sourceFile1.txt";
 		Assembler assemble = new Assembler(sourceFile);
-		assemble.read_file();
-	
-		int num = 10;
-		System.out.println();
-		System.out.println(assemble.convertToOctal(num));
+		assemble.first_pass();
 	}
 }
